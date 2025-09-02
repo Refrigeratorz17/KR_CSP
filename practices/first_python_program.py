1350906b67482d2c148219dfc2257ccc4951bae4
Refrigeratorz17 <kayzee.royer@ucas-edu.net>
2025-09-02 13:39:31 -0600

    programmed variables practice

---
+++ practices/first_python_program.py
@@ -0,0 +1,4 @@
+# KR 6th First Python Program 
+
+name = input("Hello! What is your name?\n") 
+print("Hello", name,"nice to meet you!") 
--- practices/variables_practice.py
+++ practices/variables_practice.py
@@ -1,5 +1,15 @@
 # KR 6th Variables Practice 
 
-name = "Kayzee Royer"
-number = 1-10 
-Number = 100-1000
+name = "Kayzee"
+number = 7
+num = 500
+breakfast = "cereal" 
+color = "orange"
+school = "UCAS" 
+year = 2025
+eye = "green" 
+age = 14
+subject = "biology" 
+print("my name is",name,"a number from one to ten is",number,"a number from one hundred to one thousand is",num)
+print("I had", breakfast,"for breakfast","my favorite color is",color, "The school i go to is",school)
+print("It is",year,"my eyes are",eye,"I am",age,"years old","and my favorite subject at school is",subject)
