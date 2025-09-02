15c62a464782d00163fd386fa847872593f7fc02
Refrigeratorz17 <kayzee.royer@ucas-edu.net>
2025-09-02 12:50:12 -0600

    variables practise

---
+++ variables.py
@@ -0,0 +1,18 @@
+# KR 6th Variables Notes 
+
+#Variable 
+subject = "CSP" 
+pi = 3.14159
+color = "purple" 
+
+print(pi)
+
+#snake_case (all lower case, replace space with underscore)
+baseball_team = "Red Sox" 
+#camelCase (no spaces, first letter of all words except the first are capitilized)
+baseballTeam = "Pirates"
+#PascalCase (capitilize first letter of every word, no space)
+BaseballTeam = "Giants" 
+
+#reassign a variable =
+pi = 3.141
