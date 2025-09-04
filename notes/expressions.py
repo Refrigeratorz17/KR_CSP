1b2709c6503286d31f0c4068e85c9c8df79c951b
Refrigeratorz17 <kayzee.royer@ucas-edu.net>
2025-09-04 13:43:09 -0600

    eexpression notes

---
+++ notes/expressions.py
@@ -0,0 +1,42 @@
+# KR 6th Expressions Notes
+
+#What is an algorithm? 
+    #set of instructions to accomplish a task
+    #Expression is a collection of numbers and symbols in order to get a result
+    #Going through the exact same steps
+name = input("What is your name? ")
+print("hello", name)
+
+#algorithm for area
+length = 5
+width = 20 
+area = length * width
+print(length, "X", width, "=", area) 
+#List steps in an algorithm
+    # 1. variables/pieces of info needed
+    # 2. process our info
+    # 3.provide an output
+
+#finding an average age from a group of 4
+#1
+age1 = 20
+age2 = 12
+age3 = 5
+age4 = 50
+#2
+average = (age1+age2+age3+age4)/4
+#3
+print("the average of these ages", age1,"-",age2,"-",age3,"-",age4,"-","is", average)
+#List all of the different mathematical operators
+num_one = 10
+num_two = 6
+
+print("addition:", num_one + num_two)
+print("subtraction:", num_one - num_two)
+print("multiplication:", num_one * num_two)
+print("division:", num_one / num_two)
+print("exponents:", num_one ** num_two)
+print("integer divison:", num_one // num_two)
+print("modulo (remainder):", num_one % num_two)
+#List all of the different assignment operators 
+    #
