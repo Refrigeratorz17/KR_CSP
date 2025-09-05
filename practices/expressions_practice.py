04f2a5d4f1335abab004f21818d8738cc7f37f32
Refrigeratorz17 <kayzee.royer@ucas-edu.net>
2025-09-05 13:43:51 -0600

    Expressoins practice X2

--- practices/expressions_practice.py
+++ practices/expressions_practice.py
@@ -1,3 +1,4 @@
 # KR 6th Expressions Practice 
 
-print("equation =" equation,) 
+print("7-24/8*4+6=",int(7-24/8*4+6)) 
+print("18/3-7+2*5=",int(18/3-7+2*5))
---
+++ practices/financial_calculator.py
@@ -0,0 +1,10 @@
+# KR 6th Financial calculaor
+
+#amounts for each catagory, save 10% a month 
+#income:3000 10% 3000*.1=savings=300 
+# rent:1200=1200/3000 48% utilities:250 Groceries:200 transportation:500 
+#total expenses:2150 spending money=550
+#percent_rent= rent/income*100
+#total expenses- every money added 
+#spending = income-savings-total expenses 
+#float.2
