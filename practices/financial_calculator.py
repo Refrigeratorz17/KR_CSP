ff27cbf4e64416831a9e72e64a708bb0cba585fa
Refrigeratorz17 <kayzee.royer@ucas-edu.net>
2025-09-07 03:51:50 +0000

    financial calculator

--- practices/financial_calculator.py
+++ practices/financial_calculator.py
@@ -1,18 +1,25 @@
-# KR 6th Financial calculaor
+# KR 6th Financial calcualtor 
 
-#amounts for each catagory, save 10% a month 
-#income:3000 10% 3000*.1=savings=300 
-# rent:1200=1200/3000 48% utilities:250 Groceries:200 transportation:500 
-#total expenses:2150 spending money=550
-#percent_rent= rent/income*100
-#total expenses- every money added 
-#spending = income-savings-total expenses 
-#float.2
+income = round(float(input("What is your monthly income?:")), 2)
+rent = round(float(input("What is your monthly rent/mortgage?:")),2)
+utilities = round(float(input("What is your monthly utilities?:")), 2)
+groceries = round(float(input("What is you rmonthly groceries?:")), 2)
+transport = round(float(input("What is your monthly transport?:")), 2)
+savings = int(income*0.1)
 
-income = input(int("what is your monthly income?:"))
-rent = input(int("What is your monthly rent/mortgage?:"))
-utilities = input("What is your monthly utilities?:")
-groceries = input("What is your monthly groceries?:")
-transportation = input("What is you rmonthly transportation?:") 
+rent_perc = int(rent/income*100)
+util_perc = int(utilities/income*100)
+groc_perc = int(groceries/income*100)
+tran_perc = int(transport/income*100)
+save_perc = int(savings/income*100)
 
-print("your rent is $", rent, "that is", rent/income*100, "percent of your income")
+print("your rent is", rent, "and that is", rent_perc, "percent of your income")
+print("your utilities is", utilities, "and that is", util_perc, "percent of your income")
+print("your groceries is", groceries, "and that is", groc_perc, "percent of your income")
+print("your tansport is", transport, "and that is", tran_perc, "percent of your income")
+print("you should save", savings, "a month, and that is", save_perc, "percent of your income")
+
+total = rent+utilities+groceries+transport+savings
+s_m = round(float(income-total), 2)
+
+print("you have", s_m, "dollars of spending money each month")
