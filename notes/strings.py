23facc8c8453790014984bb37686fb24fc15f1a5
Refrigeratorz17 <kayzee.royer@ucas-edu.net>
2025-09-08 13:44:20 -0600

    strings

--- notes/strings.py
+++ notes/strings.py
@@ -1,2 +1,31 @@
 # KR 6th Strings notes
+
 print("I did it!")
+
+#1)-What makes something a string?
+    #1.just a collection of symbols that are held together with quotation marks
+    #example-"content of string" or 'this is also a string'
+    #2.quotation marks on either side have to match
+ #Examples of strings 
+first_name = input("What is your first name?:").strip().title()
+
+last_name = input("What is your last name?:").strip().title()
+
+full_name = first_name + " " + last_name 
+
+sentance = 'The quick brown fox jumps over the lazy dog'
+
+print("Welcome to my program", full_name + "!")
+#2)-Why do we have strings?
+    #2).so we can talk to the user
+#How do stupid proofing and sanitization relate to each other?
+    #stupid proofing-
+    #sanitation-
+#What is debugging?
+#How do you debug the different types of errors?
+#Describe what each of the methods below does:
+    #find()
+    #concatenate (add)
+    #upper-makes the whole sentance upper case
+    #lower-make the whole sentance lower case
+    #strip-It gets rid of spaces at the begining and at the end 
