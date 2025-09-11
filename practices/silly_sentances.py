fb174a514fc5ac600baf7395da54a53a5d699733
Refrigeratorz17 <kayzee.royer@ucas-edu.net>
2025-09-11 02:23:37 +0000

    silly sentances

--- practices/silly_sentances.py
+++ practices/silly_sentances.py
@@ -4,6 +4,6 @@ word_one=input("Hello! Welcome to silly sentances. To start, please give me an e
 word_two=input("Give me a singular object or animal:\n").strip().lower()
 word_three=input("give me an adjective:\n").strip().lower()
 print("Here is your story!\n")
-print("I woke up feeling" + " " + word_one +"," + " " + "the sun was shining through my window.")
+print("I woke up feeling", word_one + ",", "the sun was shining through my window.")
 print("All of a sudden a", word_two,  "flew and crashed through my window!")
 print("I picked it up, it was", word_three, "so I threw it back out the already shattered window.")
