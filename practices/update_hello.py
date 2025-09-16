172c787070eef6ebcef779ccbf202cd2314ac651
Refrigeratorz17 <kayzee.royer@ucas-edu.net>
2025-09-16 23:59:22 +0000

    hello world finish

--- practices/update_hello.py
+++ practices/update_hello.py
@@ -1,15 +1,10 @@
-# KR 6th Update of Hello World
-
-#name = input("Hello! What is your name?\n") 
-#print("Hello", name,"nice to meet you!") 
+# KR 6th Updated hello world
 
 def welcome(name):
-    print(f"Hello {fixed}!")
-    return name
+    print(f"Hello, {name}!")
 
-#user_input = input("What is your name?\n")
-#user_respond = welcome(user_input)
- 
-user_name = input("what is your name?\n")
-name_response = welcome(user_name)
-    
+welcome("Jackie")
+welcome("Daniel")
+welcome("Skyler")
+welcome("Kayzee")
+welcome("Aleena")
