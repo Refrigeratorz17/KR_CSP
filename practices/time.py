239067e1805d8c671dcbd98a7d7c273cff9ce25a
Refrigeratorz17 <kayzee.royer@ucas-edu.net>
2025-09-18 13:43:34 -0600

    time

---
+++ practices/time.py
@@ -0,0 +1,12 @@
+# KR 6th Time of Day
+
+time = int(input("What is the hour in military time?"))
+
+if time <= 24 and time >= 18:
+    print("Good Evening")
+elif time >= 12 and time <= 17:
+    print("Good Afternoon!")
+elif time >= 0 and time <= 11:
+    print("Good Morning!")
+else:
+    print("That is not a time in military time")
