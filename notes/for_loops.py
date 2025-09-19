5721f8d37d6d8da5bdf25fdf079019209db63ba8
Refrigeratorz17 <kayzee.royer@ucas-edu.net>
2025-09-19 13:26:07 -0600

    time of day

---
+++ notes/for_loops.py
@@ -0,0 +1,29 @@
+# KR 6th Time and For Loops
+
+import datetime
+
+current = datetime.datetime.now()
+hour = current.hour 
+
+#print(f"The time in seconds since Jan 1, 1970: {epoch}")
+print(f"The time is: {current}")
+print(f"Hour is: {hour}")
+
+#lists
+siblings = ["Memphis", "Skyler", "PJ", "Adam", "Lizzie", "Casandra", "Paul", "Sebastian"["River", "Owen", "Willow", "Maylein"], 8, 3.14159] #Can hold all types of data
+print
+
+#What is a loop? 
+    #
+#What are the two types of loops?
+    #
+#What is iteration
+    #
+#What are lists? 
+    #
+#How do you make lists in python? 
+    #
+#How do you make for loops in python? 
+    #
+#How do you make while loops in python?
+    #
--- practices/time.py
+++ practices/time.py
@@ -3,7 +3,7 @@
 time = int(input("What is the hour in military time?"))
 
 if time <= 24 and time >= 18:
-    print("Good Evening")
+    print("Good Evening!")
 elif time >= 12 and time <= 17:
     print("Good Afternoon!")
 elif time >= 0 and time <= 11:
