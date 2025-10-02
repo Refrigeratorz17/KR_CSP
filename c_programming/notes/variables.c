8650d312a48157534535a92e7c6c5bbc2ee4b999
Refrigeratorz17 <kayzee.royer@ucas-edu.net>
2025-10-02 13:44:02 -0600

    c notes

---
+++ c_programming/notes/variables.c
@@ -0,0 +1,19 @@
+// KR 6th Variables Notes
+#include <stdio.h>
+
+int main(void){
+    int num = 4;
+    float pi = 3.14;
+    char grade = 'a';  //will only hold one letter
+    char name[]= "Kayzee"; //A list, can hold a bunch of characters
+    //bool passing = true;
+    printf("%d", num); 
+
+    return 0;
+}
+
+//What is the main difference between declaring variables in Python and C? -
+    //set what type of data the variable it will hold
+//In C, what is the purpose of specifying a data type when declaring a variable?
+    //static typing (c) -> data types (for variables don't change) dynamic (python).
+
