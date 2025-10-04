bd4d28075ac203cf007dbe5602c16fbd9165f9cb
Refrigeratorz17 <kayzee.royer@ucas-edu.net>
2025-10-04 02:35:38 +0000

    practices

---
+++ c_programming/practices/first_program.c
@@ -0,0 +1,11 @@
+// KR 6th First Program
+#include <stdio.h>
+
+int main(void){
+    char name; 
+    
+    printf("Please enter your name: ");
+    scanf("%s",&name);
+
+    printf("Hello, s% Welcome to your first C program!",name);
+}
--- c_programming/practices/variables_practice.c
+++ c_programming/practices/variables_practice.c
@@ -2,5 +2,18 @@
 #include <stdio.h>
 
 int main(void){
-    char name[] = "kayzee"
+    char name[] = "kayzee";
+    int lucky_number = 5;
+    float decimal = 400.5;
+    char breakfast[] = "strawberry banana cereal with raspberries";
+    char favorite_color[] = "orange";
+    char school_name[] = "Ucas";
+    int current_year = 2025;
+    char eye_color[] = "green";
+    int age = 15;
+    char favorite_subject[] = "Biology";
+
+    printf("My name is %s\nA lucky number is %d\n%f gpa\nI ate %s for breakfast\n%s is my favorite color\nI go to %s\nIt is currently %d\nmy eye color is %s\ni am %d years old\nand my favorite subject is %s\n",name,lucky_number,decimal,breakfast,favorite_color,school_name,current_year,eye_color,age,favorite_subject );
+
+    return 0;
 }
