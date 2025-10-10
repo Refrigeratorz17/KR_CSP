c5a79eb962ed1f733a954ab98433476141b6468d
Refrigeratorz17 <kayzee.royer@ucas-edu.net>
2025-10-10 21:29:10 +0000

    conditionals practices

--- c_programming/practices/old_enough.c
+++ c_programming/practices/old_enough.c
@@ -1,7 +1,27 @@
 //KR 6th Old Enough
 #include <stdio.h>
+#include <strings.h>
 
 int main(void){
+    //vote=18, drive=16, learners=15,school=6
+    int age;
+
+    printf("What is your age:\n");
+    scanf("%d",&age);
+
+    if(age >= 130){
+        printf("You are dead...\n");
+    }else if(age >= 18){
+        printf("You are old enough to vote\n");
+    }else if(age >= 16){
+        printf("You are old enough to drive\n");
+    }else if(age >= 15){
+        printf("You are old enough to get a drivers license\n");
+    }else if(age >= 4){
+        printf("You are old enough to go to school\n");
+    }else{
+        printf("You are too young\n");
+    }
 
     return 0;
 }
--- c_programming/practices/time_of_day.c
+++ c_programming/practices/time_of_day.c
@@ -2,6 +2,20 @@
 #include <stdio.h>
 
 int main(void){
+    int time;
+
+    printf("What is the hour in military time:\n");
+    scanf("%d",&time);
+
+    if(time <= 24 && time >= 18){
+        printf("Good evening!\n");
+    }else if(time >= 12 && time <= 17){
+        printf("Good afternoon!\n");
+    }else if(time >= 0 && time <= 11){
+        printf("Good Morning!\n");
+    }else{
+        printf("That is not a time in military time.");
+    }
 
     return 0;
 }
