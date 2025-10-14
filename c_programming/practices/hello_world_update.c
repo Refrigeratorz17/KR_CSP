b53f5c9b499504b01f788756e1fe0aa142acb08f
Refrigeratorz17 <kayzee.royer@ucas-edu.net>
2025-10-14 20:37:01 +0000

    hello world update

--- c_programming/practices/hello_world_update.c
+++ c_programming/practices/hello_world_update.c
@@ -1,2 +1,15 @@
 //KR 6th Hello World Update
 #include <stdio.h>
+
+void name(char*name){
+    printf("Hello %s!\n",name);
+}
+
+int main(void){
+    name("Aleena");
+    name("Memphis");
+    name("Daniel");
+    name("Jackie");
+    name("Paul");
+
+}
